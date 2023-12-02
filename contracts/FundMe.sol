491017f20d9d7763a4e5f730af9c68389153cf50
Awacs <tomas.kosub@gmail.com>
2023-12-02 09:42:24 +0100

    finished contracts

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,94 +1,69 @@
 // SPDX-License-Identifier: MIT
-// 1. Pragma
 pragma solidity ^0.8.7;
-// 2. Imports
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-// 3. Interfaces, Libraries, Contracts
 error FundMe__NotOwner();
 
-/**@title A sample Funding Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample funding contract
- * @dev This implements price feeds as our library
- */
 contract FundMe {
-    // Type Declarations
-    using PriceConverter for uint256;
 
-    // State variables
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    address private immutable i_owner;
+    using PriceConverter for uint;
+
+    uint public constant MINIMUM_USD = 50 * 1 ether;
+    address public immutable i_owner;
     address[] private s_funders;
-    mapping(address => uint256) private s_addressToAmountFunded;
+    mapping(address => uint) private s_addressToFundedAmount;
     AggregatorV3Interface private s_priceFeed;
 
-    // Events (we have none!)
-
-    // Modifiers
     modifier onlyOwner() {
-        // require(msg.sender == i_owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        if ( msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
         _;
     }
 
-    constructor(address priceFeed) {
-        s_priceFeed = AggregatorV3Interface(priceFeed);
+    constructor(address _priceFeed) {
+        s_priceFeed = AggregatorV3Interface(_priceFeed);
         i_owner = msg.sender;
     }
 
-    /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
-        require(
-            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
-            "You need to spend more ETH!"
-        );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        s_addressToAmountFunded[msg.sender] += msg.value;
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "Insufficient amount of ETH provided.");
+
         s_funders.push(msg.sender);
+        s_addressToFundedAmount[msg.sender] += msg.value;
     }
 
     function withdraw() public onlyOwner {
-        address[] memory funders = s_funders;
-        // mappings can't be in memory, sorry!
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            address funder = funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
+        for (uint i; i < s_funders.length; i++) {
+            address funder = s_funders[i];
+            s_addressToFundedAmount[funder] = 0;
         }
+
         s_funders = new address[](0);
-        // payable(msg.sender).transfer(address(this).balance);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
-        require(success);
+        (bool sent, ) = payable(i_owner).call{ value: address(this).balance }("");
+        require(sent, "The transaction failed.");
     }
 
-    /** @notice Gets the amount that an address has funded
-     *  @param fundingAddress the address of the funder
-     *  @return the amount funded
-     */
-    function getAddressToAmountFunded(
-        address fundingAddress
-    ) public view returns (uint256) {
-        return s_addressToAmountFunded[fundingAddress];
+    function getAddressToAmountFunded(address _funder) public view returns(uint) {
+        return s_addressToFundedAmount[_funder];
     }
 
-    function getVersion() public view returns (uint256) {
+    function getVersion() public view returns(uint) {
         return s_priceFeed.version();
     }
 
-    function getFunder(uint256 index) public view returns (address) {
-        return s_funders[index];
+    function getFunder(uint _index) public view returns(address) {
+        return s_funders[_index];
     }
 
-    function getOwner() public view returns (address) {
+    function getOwner() public view returns(address) {
         return i_owner;
     }
 
-    function getPriceFeed() public view returns (AggregatorV3Interface) {
+    function getPriceFeed() public view returns(AggregatorV3Interface) {
         return s_priceFeed;
     }
+
 }
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -4,27 +4,18 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-  function getPrice(AggregatorV3Interface priceFeed)
-    internal
-    view
-    returns (uint256)
-  {
-    (, int256 answer, , , ) = priceFeed.latestRoundData();
-    // ETH/USD rate in 18 digit
-    return uint256(answer * 10000000000);
-  }
 
-  // 1000000000
-  // call it get fiatConversionRate, since it assumes something about decimals
-  // It wouldn't work for every aggregator
-  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed)
-    internal
-    view
-    returns (uint256)
-  {
-    uint256 ethPrice = getPrice(priceFeed);
-    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
-    // the actual ETH/USD conversation rate, after adjusting the extra 0s.
-    return ethAmountInUsd;
-  }
+    function getPrice(AggregatorV3Interface _priceFeed) internal view returns(uint) {
+        (, int answer, , , ) = _priceFeed.latestRoundData();
+
+        // Answer has 8 decimals, ETH has 18 decimals => 10e10 to have the same units
+        return uint(answer * 10e10);
+    }
+
+    function getConversionRate(uint ethAmount, AggregatorV3Interface _priceFeed) internal view returns(uint) {
+        uint ethPrice = getPrice(_priceFeed);
+        uint ethAmountInUsd = ( ethPrice * ethAmount ) / 1 ether;
+        return ethAmountInUsd;
+    }
+
 }
